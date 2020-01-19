d10bb6fbb30b83e08d94172ac7c0869e6d4318b9
RhodiColossus <you@example.com>
2020-01-19 02:15:54 -0800

    Добавил возвращение короткого URL и его генерацию

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"encoding/json"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/asaskevich/govalidator"
 	"github.com/gorilla/mux"
@@ -22,8 +27,52 @@ type ShortU struct {
 var longUrls []LongU
 var shortUrls []ShortU
 
+func GetMD5Hash(text string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(text))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
 //Принимает длинный url ,возвращает короткий
 func postShortURL(w http.ResponseWriter, request *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	l := LongU{}
+	s := ShortU{}
+	_ = json.NewDecoder(request.Body).Decode(&l)
+
+	validURL := govalidator.IsURL(l.HUrl)
+	if validURL != true {
+		l.HUrl = "incorrect url" //вывод ошибки о некоректности URL
+	}
+
+	resporse := URLgenerate()
+	s.HUrl = resporse
+
+	json.NewEncoder(w).Encode(s)
+}
+
+//URLgenerate = Генерация короткого URL
+func URLgenerate() string {
+	shortURL := "http://qwe.com/"
+
+	rand.Seed(time.Now().UTC().UnixNano())
+	var bytes int
+	bytes = rand.Intn(100000000)
+
+	var s string
+	s = strconv.Itoa(bytes)
+
+	md5String := GetMD5Hash(s)
+	result := md5String[0:5]
+
+	shortURL += result
+
+	return shortURL
+}
+
+//Принимает Короткий url ,возвращает Длинный
+func postLongURL(w http.ResponseWriter, request *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	l := LongU{}
@@ -48,9 +97,8 @@ func getShortURL(w http.ResponseWriter, request *http.Request) {
 func main() {
 	router := mux.NewRouter()
 
-	shortUrls = append(shortUrls, ShortU{HUrl: "short-1"})
-	router.HandleFunc("/short", getShortURL).Methods("GET")
 	router.HandleFunc("/short", postShortURL).Methods("POST")
+	router.HandleFunc("/long", postShortURL).Methods("POST")
 
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":8001", router))
 }
